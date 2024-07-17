f34ce4f2618ed134a0ebcce74a9bc497c44276c8
a.nino <a.nino@tcondesa.com.mx>
2024-07-16 18:24:37 -0600

    aqui van 2 navbars

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Navbar1 from './components/Navbar-1/Navbar';
+// import Navbar2 from './components/Navbar-2/Navbar'
+// import Navbar3 from './components/Navbar-3/Navbar';
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar1 />
+      {/* <Navbar2 /> */}
+      {/* //<Navbar3 /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import "./components/Navbar-1/index.css";
+//import "./components/Navbar-2/index.css";
+//import "./components/Navbar-3/index.css";
+
+//import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
